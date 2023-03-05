a5994c1ba4b8cea3a9d3ed5375a7915f18474603
ADDB <addb1234567890@gmail.com>
2023-03-05 23:52:46 +0100

    Image procession script (improved by adding upscaling and downsizing again)

--- scripts/imageExtraction.py
+++ scripts/imageExtraction.py
@@ -1,31 +1,37 @@
 import skimage
 import matplotlib.pyplot as plt
+from PIL import Image
 from shapely.geometry import Point, Polygon
+import numpy as np
 
 testFiles = ["Miner's Boots", "Carpenter's Boots", "Paladin's Boots", "Paladin's Gauntlets",
              "Snowman Hands", "Miner's Mitts"]
-testFiles = testFiles[2:]
 
 for file in testFiles:
     fullImage = skimage.io.imread(f"../data/{file}.png")
+    img = Image.fromarray(fullImage)
+    w, h = img.size
+    zoom = 4
+    fullImage = np.array(img.resize((zoom*w, zoom*h), Image.LANCZOS))
     image = fullImage[..., 0]
     fix, ax = plt.subplots()
     contours = skimage.measure.find_contours(image, level=150)
-    indexes = []
     poly = None
-    areamin = -1
+    maxindex = -1
+    areamax = -1
+    dis = 0.3
     for i in range(len(contours)):
-        poly = Polygon(contours[i]).buffer(distance=-0.5)
-        if poly.contains(Point(image.shape[0]/2, image.shape[1]/2)) \
-                and not poly.contains(Point(10, image.shape[1]/2)):
-            indexes.append(i)
-            if areamin == -1 or poly.area < areamin:
-                areamin = poly.area
-    for i in indexes:
-        poly = Polygon(contours[i]).buffer(distance=-0.5)
-        if poly.area == areamin:
-            contour = contours[i]
-            break
+        poly = Polygon(contours[i]).buffer(distance=dis)
+        if poly.contains(Point(image.shape[0]/2, image.shape[1]/2)) and not \
+                (poly.contains(Point(zoom*5, image.shape[1]/2))
+                 and poly.contains(Point(image.shape[0]/2 - zoom*5, image.shape[1]/2))
+                 and poly.contains(Point(image.shape[0]/2, zoom*5))
+                 and poly.contains(Point(image.shape[0]/2, image.shape[1]/2 - zoom*5))):
+            if poly.area > areamax:
+                areamax = poly.area
+                maxindex = i
+    contour = contours[maxindex]
+    poly = Polygon(contour).buffer(distance=dis)
 
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
@@ -33,8 +39,6 @@ for file in testFiles:
             if not poly.contains_properly(p):
                 fullImage[i][j] = (255, 255, 255, 0)
 
-    # ax.imshow(fullImage)
-    # ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-    # plt.show()
-    skimage.io.imsave(f"../data/{file}_processed.png", fullImage)
-    # break
+    finished = Image.fromarray(fullImage).resize((w, h), Image.LANCZOS)
+    ax.imshow(finished)
+    finished.save(f"../data/{file}_processed.png")
